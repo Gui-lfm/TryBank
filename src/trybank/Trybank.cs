2addae11fa6db5bad8399b852051ec83b6a67915
Guilherme Lucena <guilherme.lucena17@gmail.com>
2024-06-28 16:40:15 -0300

    feat(TryBank.cs): req06
    funcionalidade de sacar dinheiro implementada.

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -103,7 +103,21 @@ public class TrybankLib
     // 6. Construa a funcionalidade de sacar dinheiro
     public void Withdraw(int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+
+        int userBalance = Bank[loggedUser, 3];
+
+        if (userBalance - value < 0)
+        {
+            throw new InvalidOperationException("Saldo insuficiente");
+        }
+        else
+        {
+            Bank[loggedUser, 3] = userBalance - value;
+        }
     }
 
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
